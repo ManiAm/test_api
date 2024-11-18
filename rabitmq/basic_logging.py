98ec5a912e7644d7978887af1aae4160d70394fe
maniam <maniam@rabbitmq.myguest.virtualbox.org>
2024-11-18 12:28:57 -0800

    small update

--- rabitmq/basic_logging.py
+++ rabitmq/basic_logging.py
@@ -1,4 +1,10 @@
 import pika
+import logging
+
+LOG_FORMAT = ('%(levelname) -10s %(asctime)s %(name) -30s %(funcName) -35s %(lineno) -5d: %(message)s')
+logging.basicConfig(level=logging.INFO, format=LOG_FORMAT)
+
+log = logging.getLogger(__name__)
 
 # Setup connection to RabbitMQ server
 connection_param = pika.ConnectionParameters('localhost')
@@ -11,7 +17,7 @@ channel.exchange_declare(exchange=exchange_name, exchange_type='direct')
 
 # Define a callback for returned messages
 def on_message_returned(channel, method, properties, body):
-    print(f"Message returned: {body}")
+    log.info(f"Message returned: {body}")
 
 channel.add_on_return_callback(on_message_returned)
 
@@ -26,7 +32,7 @@ for idx in range(100):
         mandatory=True
     )
 
-    print(f"Sent message: {message}")
+    log.info(f"Sent message: {message}")
 
     # call this method to handle RabbitMQ events (e.g., messages arriving)
     connection.process_data_events(1)
